36370810a34dd9e080d466329694d9b5e0d0e9a7
Rachael <rachung23@gmail.com>
2026-01-15 20:33:11 -0500

    About Page, Photos, Project Cards

---
+++ projects.js
@@ -0,0 +1,21 @@
+const projects = {
+    minesweeperButBetter: {
+        title: 'Minesweeper But Better',
+        who: 'Solo project',
+        what: 'A final project created in my intro to Python class (15-112) that explores minesweeper with three gamemodes: regular, compeitive, and musical. Complete with clean UI, custom themes, and a store." ',
+        when: 'Fall 2025',
+        why: 'Minesweeper is a classic game that I enjoy quite a lot, but found aesthetically uninteresting and repetitive, thus leading to many themes and gamemodes.',
+        how: 'Using Python, CMU CS 112 Graphics, and built within CS Academy',
+        embed: 'https://academy.cs.cmu.edu/sharing/linenDuck437082'
+    },
+    project2: {
+        title: 'Video Project',
+        who: 'Team of 3',
+        what: 'A documentary about...',
+        when: 'Spring 2024',
+        why: 'To raise awareness...',
+        how: 'Filmed with Canon...',
+        embed: 'https://www.youtube.com/embed/VIDEO_ID'
+    }
+    // ... all your projects
+};

43201512e8bc54267d28a2497836f6c1f92bc146
Rachael <rachung23@gmail.com>
2026-01-16 12:27:24 -0500

    starting to add projects

--- projects.js
+++ projects.js
@@ -3,7 +3,7 @@ const projects = {
         title: 'Minesweeper But Better',
         who: 'Solo project',
         what: 'A final project created in my intro to Python class (15-112) that explores minesweeper with three gamemodes: regular, compeitive, and musical. Complete with clean UI, custom themes, and a store." ',
-        when: 'Fall 2025',
+        when: 'Fall 2025 in one week',
         why: 'Minesweeper is a classic game that I enjoy quite a lot, but found aesthetically uninteresting and repetitive, thus leading to many themes and gamemodes.',
         how: 'Using Python, CMU CS 112 Graphics, and built within CS Academy',
         embed: 'https://academy.cs.cmu.edu/sharing/linenDuck437082'

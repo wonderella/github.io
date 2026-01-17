e04b36ff62762703e04be4cd1ce767b8bee3ebaa
Rachael <rachung23@gmail.com>
2026-01-17 18:35:59 -0500

    adding projects

--- projects.js
+++ projects.js
@@ -6,9 +6,9 @@ const projects = {
         when: 'Fall 2025 in one week',
         why: 'Minesweeper is a classic game that I enjoy quite a lot, but found aesthetically uninteresting and repetitive, thus leading to many themes and gamemodes.',
         how: 'Using Python, CMU CS 112 Graphics, and built within CS Academy',
-        embed: 'https://academy.cs.cmu.edu/sharing/linenDuck437082'
+        embed: 'https://academy.cs.cmu.edu/sharing/linenDuck437082',
         buttonText: 'See on GitHub',
-        buttonLink: 
+        buttonLink: 'https://github.com/wonderella/minesweeperButBetter/tree/main'
     },
     wishWeekReveal: {
         title: 'Wish Week Reveal',
@@ -17,9 +17,50 @@ const projects = {
         when: 'Spring 2025',
         why: 'Wish Week is a fundraising event for Make-A-Wish Colorado where we raise money for a child with a critical illness. This video sets off the entire fundraising week.',
         how: 'Coordinating filming times and locations, shooting footage, and editing clips.',
-        embed: "https://www.youtube.com/embed/-YarKRqx_uU"
+        embed: "https://www.youtube.com/embed/-YarKRqx_uU",
         buttonText: 'Watch on YouTube',
         buttonLink: 'https://youtu.be/-YarKRqx_uU'
+    },
+    mechEFinalProject: {
+        title: 'Pill Dispenser Mechanism',
+        who: 'Group project of 4 in Fundamentals of Mechanical Engineering (24-101)',
+        what: 'A pill dispenser built to deliver marbles (as pills) at a set interval',
+        when: 'Fall 2025 over 3 weeks',
+        why: 'A final project applying coursework in CAD, manufacturing, teamwork, and design to create a functional mechanism',
+        how: 'Using SolidWorks for CAD, laser cutting and 3D printing for manufacturing, and teamwork to design and iterate the mechanism',
+        images: ['images/mechEFinal.jpg', 'images/mechEFinal2.jpg', 'images/mechEFinalCAD.jpg']
+    },
+    flashlight: {
+        title: 'Hand-Powered Flashlight',
+        who: 'Solo project within Fundamentals of Mechanical Engineering (24-101)',
+        what: 'A hand-powered flashlight using gear systems to convert rotational energy into electrical energy to power an LED light',
+        when: 'Fall 2025 over 1 week',
+        why: 'A project to calculate gear ratios and design a gear to reach a target output speed from provided CAD file.',
+        how: 'Using SolidWorks for CAD, laser cutting and 3D printing for manufacturing, and calculations to determine gear ratios',
+        images: ['images/flashlight.jpg', 'images/flashlightCAD.jpg']
+    },
+    countingLines: {
+        title: 'Counting Lines Arduino for Mobot',
+        who: 'Solo project within Fundamentals of Mechanical Engineering (24-101)',
+        what: 'A programmed Arduino to count lines crossed by a Mobot robot using an IR sensor and stop after a certain distance',
+        when: 'Fall 2025 over 1 week',
+        why: 'A project to practice physically assembling an Arduino circuit and programming basic functions',
+        how: 'Using Arduino IDE and a breadboard to create the circuit and program the Arduino for the Mobot',
+        images: ['images/mobot.jpg'],
+        buttonText: 'See Code on GitHub',
+        buttonLink: 'https://github.com/wonderella/controlSystemsChallenge'
+    },
+    wishWeekBranding: {
+        title: 'Wish Week Branding',
+        who: 'Director for Wish Week 2025 Brand Identity at Chaparral High School',
+        what: "Branding and promotional materials for Chaparral High School's Wish Week. Hoodie products designed sold out in 1 day.",
+        when: 'Spring 2025',
+        why: 'To create a cohesive visual identity for a fundraising event, designing social media graphics and logos.',
+        how: 'Designed using social media graphics with Canva and logos with Linearity Curve and help from Lance Kirkpatrick',
+        images: ['images/wishWeekRevealThumbnail.jpg'],
+        buttonText: 'See Code on GitHub',
+        buttonLink: 'https://github.com/wonderella/controlSystemsChallenge'
     }
+
 }
     // ... all your projects
